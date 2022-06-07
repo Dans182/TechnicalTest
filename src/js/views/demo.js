8d6cc3b05e9751e67b14711d9400e47050a10de5
Dans182 <dgaiteiro@gmail.com>
2022-06-07 19:35:35 +0000

    v2

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -1,43 +1,54 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
-
+import React from "react";
+import { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
-
-import "../../styles/demo.css";
+import "../../styles/home.css";
 
 export const Demo = () => {
-	const { store, actions } = useContext(Context);
+	const { actions, store } = useContext(Context);
 
-	return (
-		<div className="container">
-			<ul className="list-group">
-				{store.demo.map((item, index) => {
-					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
-							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
-								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
-						</li>
-					);
-				})}
-			</ul>
-			<br />
-			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
-			</Link>
-		</div>
-	);
-};
+	useEffect(() => {
+		getCategories();
+	  }, []);
+
+
+	  const getCategoriesHats = async () => {
+		try {
+		  const resp = await fetch("https://api.thecatapi.com/v1/images/search?category_ids=2", {
+			method: "GET",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(),
+		  });
+		  const dataCategoriesHats = await resp.json();
+		} catch (e) {
+		  alert("ERROR");
+		}
+	  };	  
+
+	  
+return(
+	<div className="text-center mt-5">
+		<p>
+			<button
+              className="btn btn-outline-success"
+              type="button"
+              onClick={() => {
+                getBreeds();
+              }}
+            >
+              Breeds
+            </button>
+		</p>
+		<p>
+			<button
+              className="btn btn-outline-success"
+              type="button"
+              onClick={() => {
+                getCategoriesHats();
+              }}
+            >
+              Catogory - Hats
+            </button>
+		</p>
+
+	</div>
+)};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -23,6 +23,19 @@ export const Home = () => {
 		}
 	  };
 
+	  const getCategoriesHats = async () => {
+		try {
+		  const resp = await fetch("https://api.thecatapi.com/v1/images/search?category_ids=2", {
+			method: "GET",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(),
+		  });
+		  const dataCategoriesHats = await resp.json();
+		} catch (e) {
+		  alert("ERROR");
+		}
+	  };	  
+
 	  const getBreeds = async () => {
 		try {
 		  const resp = await fetch("https://api.thecatapi.com/v1/breeds", {
@@ -34,7 +47,8 @@ export const Home = () => {
 		} catch (e) {
 		  alert("ERROR");
 		}
-	  };	  
+	  };
+
 
 return(
 	<div className="text-center mt-5">
@@ -58,6 +72,17 @@ return(
               }}
             >
               Breeds
+            </button>
+		</p>
+		<p>
+			<button
+              className="btn btn-outline-success"
+              type="button"
+              onClick={() => {
+                getCategoriesHats();
+              }}
+            >
+              Catogory - Hats
             </button>
 		</p>
 
