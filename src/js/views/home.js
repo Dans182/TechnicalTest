f7a65e4ca24c67e40bd3ebc0d3bf02acd9a233ee
Dans182 <dgaiteiro@gmail.com>
2022-06-08 18:43:30 +0000

    home listo

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,31 +1,44 @@
 import React from "react";
-import { Fragment, useContext, useEffect } from "react";
-import { Context } from "../store/appContext";
+import { Fragment } from "react";
 import { Link } from "react-router-dom";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { actions, store } = useContext(Context);
+
 
 return(
-	<div className="text-center mt-5">
-		<p>
-			<Link to="categories"><button
-              className="btn btn-outline-success"
-              type="button"
-            >
-              Categories
-            </button>
-			</Link>
-		</p>
-		<p>
-		<Link to="breeds"><button
-              className="btn btn-outline-success"
-              type="button"
-            >
-              Breeds
-            </button>
-			</Link>
-		</p>
-	</div>
+    <Fragment>
+      <div className="cat-content">
+        <img
+          className="cat-img"
+          src="https://images.pexels.com/photos/599492/pexels-photo-599492.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        />
+        <div className="title">
+          <h3>Cats APP</h3>
+        </div>
+        <div className="content-buttons">
+          <p>
+            <Link to="categories">
+              <button
+                className="buttons"
+                type="button"
+              >
+                Categories
+              </button>
+            </Link>
+			</p>
+          <p>
+            <Link to="breeds">
+              <button
+                className="buttons"
+                type="button"
+
+              >
+                Breeds
+              </button>
+            </Link>
+          </p>
+        </div>
+      </div>
+    </Fragment>
 )};
