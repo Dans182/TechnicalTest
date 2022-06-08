e136c5f4c4f2b42b824881881614fd07c30737af
Dans182 <dgaiteiro@gmail.com>
2022-06-08 19:24:22 +0000

    imagedescription styles done

--- src/js/views/imagecategories.js
+++ src/js/views/imagecategories.js
@@ -1,25 +1,31 @@
 import React from "react";
-import { useContext, useEffect } from "react";
-import { useParams } from 'react-router-dom';
+import { useContext, useEffect, Fragment } from "react";
+import { useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 
 export const Imagecategories = () => {
-	const { actions, store } = useContext(Context);
-	let { id } = useParams();
+  const { actions, store } = useContext(Context);
+  let { id } = useParams();
 
-    useEffect(() => {
-        actions.getImageCategories(id);
-      }, []);
+  useEffect(() => {
+    actions.getImageCategories(id);
+  }, []);
 
-return(
-	<div className="text-center mt-5">
-		{store.imagecategories.map((e)=>{
-		return(
-        <div key={e.id}>
-            
-        <img src={e.url} style={{"width": "50%", "height": "50%"}}></img>
-         
-		</div>)
-		})}
-	</div>)}
+  return (
+    <Fragment>
+<div className="cat-content">
+        {store.imagecategories.map((e) => {
+          return (
+            <div key={e.id}>
+              <img className="cat-img" src={e.url}></img>
+			  <div className="title">
+          <h3>Cats in {e.categories[0].name}</h3>
+        </div>
+            </div>
+          );
+        })}
+      </div>{" "}
+    </Fragment>
+  );
+};
